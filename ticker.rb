fd3f3dcb64a72cfa034f755512710f777c08133f
Justin Moulton <moultonjust@gmail.com>
2019-01-08 11:54:55 -0500

    Small updates

--- ticker.rb
+++ ticker.rb
@@ -3,15 +3,23 @@ require 'terminal-table'
 require 'rainbow'
 
 class Ticker
-  SYMBOLS = %w(SPY NVDA AMD INTC SNE AAPL TSLA MSFT AMZN FBGRX DJI)
+  SYMBOLS = %w(AMD SPY DIS SQ NFLX FTEC MSFT WMT NVDA INTC SNE GOOGL TSLA DJI FB)
+  CURRENT = %w(AAPL AMZN DAL)
 
   def self.run
     while true
       system("clear")
-      stocks = StockQuote::Stock.quote(SYMBOLS)
+      stocks = StockQuote::Stock.quote(SYMBOLS.sort_by(&:downcase))
+      current = StockQuote::Stock.quote(CURRENT.sort_by(&:downcase))
 
       table = Terminal::Table.new(:headings => ['Company', 'Close', 'Open', 'Last Price', '% change']) do |t|
         stocks.each do |stock|
+          t << [stock.company_name, stock.close, stock.open, Rainbow(stock.latest_price).cyan, self.change(stock.change_percent)]
+        end
+
+        t << :separator
+
+        current.each do |stock|
           t << [stock.company_name, stock.close, stock.open, Rainbow(stock.latest_price).cyan, self.change(stock.change_percent)]
         end
       end
